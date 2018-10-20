e49a993dd8b4f08cbf392b185dfba5cd3b7ed3f6
Michael <mrmichaelgraver@gmail.com>
2018-10-20 15:12:25 -0600

    Now can login if users has existing account.

--- familyjournal/routes/index.js
+++ familyjournal/routes/index.js
@@ -1,26 +1,73 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var registration = require("./registration");
+const registration = require("./registration");
+const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const config = require("../serverConfig");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+const pool = new Pool(config);
+const saltRounds = 10;
 
-	if(!req.session.userID)
-  		res.render('home', { Navbar: 'Login_Navbar', LoginName: ''});
-  	else {
-  		let first = req.session.first_name;
-  		let last = req.session.last_name;
-  		res.render('home', { Navbar: 'Logout_Navbar', LoginName: first + ' ' + last});
-  	}
+pool.on("error", (err, client) => {
+	console.error("Unexpected error on idle client", err);
+	process.exit(-1);
 });
 
-router.get('/logout', (req, res, next) => {
+/* GET home page. */
+router.get("/", function(req, res, next) {
+	if (!req.session.userID)
+		res.render("home", { Navbar: "Login_Navbar", LoginName: "" });
+	else {
+		let first = req.session.first_name;
+		let last = req.session.last_name;
+		res.render("home", {
+			Navbar: "Logout_Navbar",
+			LoginName: first + " " + last
+		});
+	}
+});
+
+router.post("/login", (req, res, next) => {
+	var email = req.body.email.trim();
+	var password = req.body.password_1.trim();
+	var getAccount = "SELECT * FROM users WHERE email = $1";
+
+	pool.connect((err, client, done) => {
+		if (err) throw err;
+		client.query(getAccount, [email], (err, qRes) => {
+			done();
+
+			if (err) {
+				console.log(err.stack);
+				res.status(500).send();
+			} else {
+				console.log(qRes.rows[0]);
+				let hash = qRes.rows[0].password;
+				bcrypt.compare(password, hash, function(err, pRes) {
+					if (pRes) {
+						req.session.userID = qRes.rows[0].id;
+						req.session.first_name = qRes.rows[0].first;
+						req.session.last_name = qRes.rows[0].last;
+						res.redirect("../"); //Go back to home.
+					}
+					else
+					{
+						console.log("Invalid password");
+						res.redirect("../../"); //Go back to home.
+					}
+				});
+			}
+		});
+	});
+});
+
+router.get("/logout", (req, res, next) => {
 	//Removes ression and rebuilds it on next request.
 	req.session.destroy();
 	res.redirect("../"); //Go back to home.
-})
+});
 
-router.use('/register', registration);
+router.use("/register", registration);
 
 module.exports = router;
