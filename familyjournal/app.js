dad1ba09289494e0770c7cf88bd6c6d0be7fd60c
Michael <mrmichaelgraver@gmail.com>
2018-10-20 14:16:23 -0600

    Added code to make a session and able to create a new row for a new user.

--- familyjournal/app.js
+++ familyjournal/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 
@@ -19,6 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: "smallKeys", resave: false, saveUninitialized: true}));
 app.use(express.static(path.join(__dirname, 'public'))); //All paths start in the public folder for HTML includes.
 
 app.use('/', index);
--- familyjournal/routes/registration.js
+++ familyjournal/routes/registration.js
@@ -1,28 +1,53 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const config = require('../serverConfig');
-const {Pool} = require('pg');
+const config = require("../serverConfig");
+const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const pool = new Pool(config);
 
-pool.query('SELECT * FROM users', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+pool.on("error", (err, client) => {
+	console.error("Unexpected error on idle client", err);
+	process.exit(-1);
+});
 
 /* GET registration form */
-router.get('/', (req, res, next) => {
-	res.render('register.ejs', {})
+router.get("/", (req, res, next) => {
+	res.render("register.ejs", {});
 });
 
 /*POST form data for account creation*/
-router.post('/createAccount', (req, res, next) => {
+router.post("/createAccount", (req, res, next) => {
+	console.log("createAccount");
 	console.log(req.body);
 	var first_name = req.body.first_name.trim();
 	var last_name = req.body.last_name.trim();
 	var email = req.body.email.trim();
-	var password = req.body.password.trim();
+	var password = req.body.password_1.trim();
+
+	bcrypt.hash(password, saltRounds, function(err, hash) {
+		let insertSQL =
+			"INSERT INTO users (first, last, email, password) VALUES ($1, $2, $3, $4) RETURNING *";
+
+		let insertData = [first_name, last_name, email, hash];
+
+		pool.connect((err, client, done) => {
+			if (err) throw err;
+			client.query(insertSQL, insertData, (err, qRes) => {
+				done();
+				console.log("Pool query.");
+				if (err) {
+					console.log(err.stack);
+				} else {
+					console.log(qRes.rows[0]);
+					req.session.userID = id;
+					res.redirect("/");
+				}
+			});
+		});
+	});
 });
 
 module.exports = router;
