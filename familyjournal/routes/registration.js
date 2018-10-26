7cbc86be8c9b564f6c3ee3b622dfe31ece0d8605
Michael <mrmichaelgraver@gmail.com>
2018-10-26 13:16:16 -0600

    Stopped checking out a client and did a direct querey on the pool. Cleaned up the file.

--- familyjournal/routes/registration.js
+++ familyjournal/routes/registration.js
@@ -7,43 +7,39 @@ const saltRounds = 10;
 
 /* GET registration form */
 router.get("/", (req, res, next) => {
-	res.render("register.ejs", {});
+    res.render("register.ejs", {});
 });
 
 /*POST form data for account creation*/
 router.post("/createAccount", (req, res, next) => {
-	console.log("createAccount");
-	console.log(req.body);
-	var first_name = req.body.first_name.trim().toLowerCase();
-	var last_name = req.body.last_name.trim().toLowerCase();
-	var email = req.body.email.trim().toLowerCase();
-	var password = req.body.password_1.trim();
+    console.log("createAccount");
+    console.log(req.body);
+    var first_name = req.body.first_name.trim().toLowerCase();
+    var last_name = req.body.last_name.trim().toLowerCase();
+    var email = req.body.email.trim().toLowerCase();
+    var password = req.body.password_1.trim();
 
-	bcrypt.hash(password, saltRounds, function(err, hash) {
-		let insertSQL =
-			"INSERT INTO users (first, last, email, password) VALUES ($1, $2, $3, $4) RETURNING *";
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        let insertSQL =
+            "INSERT INTO users (first, last, email, password) VALUES ($1, $2, $3, $4) RETURNING *";
 
-		let insertData = [first_name, last_name, email, hash];
+        let insertData = [first_name, last_name, email, hash];
 
-		pool.connect((err, client, done) => {
-			if (err) throw err;
-			client.query(insertSQL, insertData, (err, qRes) => {
-				done();
-				console.log("Pool query.");
-				if (err) {
-					console.log(err.stack);
-					res.status(500).send();
-				} else {
-					console.log(qRes.rows[0]);
-					req.session.userID = qRes.rows[0].id;
-					req.session.first_name = qRes.rows[0].first;
-					req.session.last_name = qRes.rows[0].last;
-					res.redirect("../../"); //Go back to home.
+        pool.query(insertSQL, insertData, (err, qRes) => {
+            console.log("Pool query.");
+            if (err) {
+                console.log(err.stack);
+                res.status(500).send();
+            } else {
+                console.log(qRes.rows[0]);
+                req.session.userID = qRes.rows[0].id;
+                req.session.first_name = qRes.rows[0].first;
+                req.session.last_name = qRes.rows[0].last;
+                res.redirect("../../"); //Go back to home.
 
-				}
-			});
-		});
-	});
+            }
+        });
+    });
 });
 
 module.exports = router;
