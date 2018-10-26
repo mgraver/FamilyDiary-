72811714e056a4f970c37609380cdff024093761
Michael <mrmichaelgraver@gmail.com>
2018-10-26 13:09:38 -0600

    Since I did not need a session but a single querey I stopped checking out a client

--- familyjournal/routes/index.js
+++ familyjournal/routes/index.js
@@ -8,65 +8,62 @@ const saltRounds = 10;
 
 //Redirect everyone to home page.
 router.get("/", (req, res, next) => {
-	res.redirect("/home");
+    res.redirect("/home");
 })
 
 /* GET home page. */
 router.get("/home", function(req, res, next) {
-	if (!req.session.userID)
-		res.render("home", { Navbar: "Login_Navbar", LoginName: "" });
-	else {
-		let first = req.session.first_name;
-		let last = req.session.last_name;
-		res.render("home", {
-			Navbar: "Logout_Navbar",
-			LoginName: first + " " + last
-		});
-	}
+    if (!req.session.userID)
+        res.render("home", { Navbar: "Login_Navbar", LoginName: "" });
+    else {
+        let first = req.session.first_name;
+        let last = req.session.last_name;
+        res.render("home", {
+            Navbar: "Logout_Navbar",
+            LoginName: first + " " + last
+        });
+    }
 });
 
 router.post("/login", (req, res, next) => {
-	var email = req.body.email.trim().toLowerCase();
-	var password = req.body.password_1.trim();
-	var getAccount = "SELECT * FROM users WHERE email = $1";
+    var email = req.body.email.trim().toLowerCase();
+    var password = req.body.password_1.trim();
+    var getAccount = "SELECT * FROM users WHERE email = $1";
 
-	pool.query(getAccount, [email], (err, qRes) => {
+    pool.query(getAccount, [email], (err, qRes) => {
 
-			if (err) {
-				console.log(err.stack);
-				res.status(500).send();
-			} else {
-				//If no results return.
-				console.log(qRes.rows.length);
-				if (qRes.rows.length < 1)
-				{
-					console.log("Entered Here");
-					res.redirect("../");
-					return;
-				}
+        if (err) {
+            console.log(err.stack);
+            res.status(500).send();
+        } else {
+            //If no results return.
+            console.log(qRes.rows.length);
+            if (qRes.rows.length < 1) {
+                console.log("Entered Here");
+                res.redirect("../");
+                return;
+            }
 
-				let hash = qRes.rows[0].password;
-				bcrypt.compare(password, hash, function(err, pRes) {
-					if (pRes) {
-						req.session.userID = qRes.rows[0].id;
-						req.session.first_name = qRes.rows[0].first;
-						req.session.last_name = qRes.rows[0].last;
-						res.redirect("../"); //Go back to home.
-					}
-					else
-					{
-						console.log("Invalid password");
-						res.redirect("../../"); //Go back to home.
-					}
-				});
-			}
-		});
+            let hash = qRes.rows[0].password;
+            bcrypt.compare(password, hash, function(err, pRes) {
+                if (pRes) {
+                    req.session.userID = qRes.rows[0].id;
+                    req.session.first_name = qRes.rows[0].first;
+                    req.session.last_name = qRes.rows[0].last;
+                    res.redirect("../"); //Go back to home.
+                } else {
+                    console.log("Invalid password");
+                    res.redirect("../../"); //Go back to home.
+                }
+            });
+        }
+    });
 });
 
 router.get("/logout", (req, res, next) => {
-	//Removes ression and rebuilds it on next request.
-	req.session.destroy();
-	res.redirect("../"); //Go back to home.
+    //Removes ression and rebuilds it on next request.
+    req.session.destroy();
+    res.redirect("../"); //Go back to home.
 });
 
 router.use("/register", registration);
