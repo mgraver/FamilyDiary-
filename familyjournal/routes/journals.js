2ecdc5639ae44cde1589d7cd2e53b5148c0ccb13
Michael <mrmichaelgraver@gmail.com>
2018-11-01 20:22:35 -0600

    User can now create an entry. A object is created from the entry, turned into json, then sent to the S3 bucket.
     Changes to be committed:
    	modified:   routes/journals.js
    	modified:   views/entryCreation.ejs
    	modified:   views/entryList.ejs

--- familyjournal/routes/journals.js
+++ familyjournal/routes/journals.js
@@ -2,39 +2,97 @@ var express = require("express");
 var router = express.Router();
 
 const pool = require("./db");
+const aws = require("../AWSconfig");
 
 //Reroute the user back to home if they arent loged in.
-router.use((req, res, next) =>{
-	if (req.session.userID)
-		next();
-	else
-		res.status(403).redirect("/home");
+router.use((req, res, next) => {
+	if (req.session.userID) next();
+	else res.status(403).redirect("/home");
 });
 
 router.get("/", (req, res, next) => {
 	const getJournals = "SELECT * FROM journals WHERE userid = $1";
 	pool.query(getJournals, [req.session.userID], (err, qRes) => {
 		console.log(qRes.rows);
-		res.render("journalList", {LoginName: req.session.full_name, Journals: qRes.rows});
+		res.render("journalList", {
+			LoginName: req.session.full_name,
+			Journals: qRes.rows
+		});
 	});
 });
 
 router.post("/createJournal", (req, res, next) => {
-	const makeJournal = "INSERT INTO journals (userid, title, description) VALUES ($1, $2, $3)";
-	console.log(req.body);
+	const makeJournal =
+		"INSERT INTO journals (userid, title, description) VALUES ($1, $2, $3)";
 	const title = req.body.title;
 	const description = req.body.description;
 
-	console.log(title);
-	console.log(description);
-
-	pool.query(makeJournal, [req.session.userID, title, description], (err, qRes) => {
-		res.redirect("/journals");
-	});
+	pool.query(
+		makeJournal,
+		[req.session.userID, title, description],
+		(err, qRes) => {
+			res.redirect("/journals");
+		}
+	);
 });
 
 router.get("/:journalId/:journalName/entries", (req, res, next) => {
-	res.render("entryList", {LoginName: req.session.full_name, JournalName:req.params.journalName});
+	let jName = req.params.journalName;
+	let jId = req.params.journalId;
+	res.render("entryList", {
+		LoginName: req.session.full_name,
+		JournalName: jName,
+		JournalId: jId
+	});
+});
+
+router.get("/:journalId/:journalName/createEntry", (req, res, next) => {
+	let jName = req.params.journalName;
+	let jId = req.params.journalId;
+	res.render("entryCreation", {
+		LoginName: req.session.full_name,
+		JournalName: jName,
+		JournalId: jId
+	});
+});
+
+router.post("/:journalId/:journalName/addEntry", (req, res, next) => {
+	const addEntrySQL =
+		"INSERT INTO entries (userid, journalid, title) VALUES ($1, $2, $3) RETURNING id";
+	var entry = {};
+	var s3 = new aws.S3();
+
+	entry.date = req.body.date;
+	entry.title = req.body.title;
+	entry.text = req.body.text;
+
+	pool.query(
+		addEntrySQL,
+		[req.session.userID, req.params.journalId, entry.title],
+		(err, qRes) => {
+			var entryJson = JSON.stringify(entry);
+			var entryBuffer = Buffer.from(entryJson);
+
+			var params = {
+				Bucket: "byui-seniorproject",
+				Body: entryBuffer,
+				Key:
+					req.session.userID +
+					"/" +
+					req.params.journalId +
+					"/" +
+					qRes.rows[0].id +
+					".json"
+			};
+
+			s3.putObject(params, (err, data) => {
+				if (err) {
+					console.log("Error", err);
+				}
+				res.redirect("entries");
+			});
+		}
+	);
 });
 
 module.exports = router;
