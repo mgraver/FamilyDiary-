9cae57cb3306c1a2626f7eed340ec5889fe61523
Michael <mrmichaelgraver@gmail.com>
2018-11-02 12:22:49 -0600

    Can now retrieve and list the the entries from the database.  Changes to be committed: 	modified:   routes/journals.js 	modified:   views/entryList.ejs

--- familyjournal/routes/journals.js
+++ familyjournal/routes/journals.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 const pool = require("./db");
 const aws = require("../AWSconfig");
+const bucketName = "byui-seniorproject";
 
-//Reroute the user back to home if they arent loged in.
+//Reroute the user back to home if they arent logged in.
 router.use((req, res, next) => {
 	if (req.session.userID) next();
 	else res.status(403).redirect("/home");
@@ -13,7 +14,6 @@ router.use((req, res, next) => {
 router.get("/", (req, res, next) => {
 	const getJournals = "SELECT * FROM journals WHERE userid = $1";
 	pool.query(getJournals, [req.session.userID], (err, qRes) => {
-		console.log(qRes.rows);
 		res.render("journalList", {
 			LoginName: req.session.full_name,
 			Journals: qRes.rows
@@ -39,10 +39,47 @@ router.post("/createJournal", (req, res, next) => {
 router.get("/:journalId/:journalName/entries", (req, res, next) => {
 	let jName = req.params.journalName;
 	let jId = req.params.journalId;
-	res.render("entryList", {
-		LoginName: req.session.full_name,
-		JournalName: jName,
-		JournalId: jId
+	var entries = [];
+	var promises = [];
+
+	var s3 = new aws.S3();
+	var params = {
+		Bucket: bucketName,
+		Delimiter: "/",
+		Prefix: req.session.userID + "/" + req.params.journalId + "/"
+	};
+
+	s3.listObjectsV2(params, (err, data) => {
+		if (err) console.log(err, err.stack);
+		for (let i = 0; i < data.Contents.length; i++) {
+			let params = {
+				Bucket: bucketName,
+				Key: data.Contents[i].Key
+			};
+
+			//Make a promise for each getObject that returnes the data
+			promises.push(
+				new Promise((success, reject) => {
+					s3.getObject(params, (err, data) => {
+						if (err) reject(err);
+						else success(data);
+					});
+				})
+			);
+		}
+
+		//Once all the get Objects are done save the metadata and return.
+		Promise.all(promises).then(results => {
+			for (let i in results) {
+				entries.push(results[i].Metadata);
+			}
+			res.render("entryList", {
+				LoginName: req.session.full_name,
+				JournalName: jName,
+				JournalId: jId,
+				entryList: entries
+			});
+		});
 	});
 });
 
@@ -74,7 +111,7 @@ router.post("/:journalId/:journalName/addEntry", (req, res, next) => {
 			var entryBuffer = Buffer.from(entryJson);
 
 			var params = {
-				Bucket: "byui-seniorproject",
+				Bucket: bucketName,
 				Body: entryBuffer,
 				Key:
 					req.session.userID +
@@ -82,7 +119,12 @@ router.post("/:journalId/:journalName/addEntry", (req, res, next) => {
 					req.params.journalId +
 					"/" +
 					qRes.rows[0].id +
-					".json"
+					".json",
+				Metadata: {
+					title: entry.title,
+					date: entry.date,
+					desci: entry.text.substring(0, 25)
+				}
 			};
 
 			s3.putObject(params, (err, data) => {
