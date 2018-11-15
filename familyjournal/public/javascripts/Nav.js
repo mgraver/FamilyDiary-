6b396dad17a0597970d66afb293aeb83ff056dbc
Michael <mrmichaelgraver@gmail.com>
2018-11-15 13:04:16 -0700

    Added to the Nav bar a friends list where the user can look at any incomeing requests and a place holder link to go to a friends list.  Example requests will be removed in time.   Changes to be committed: 	new file:   public/javascripts/Nav.js 	modified:   public/stylesheets/Nav.css 	modified:   views/Login_Navbar.ejs 	modified:   views/Logout_Navbar.ejs 	modified:   views/home.ejs

---
+++ familyjournal/public/javascripts/Nav.js
@@ -0,0 +1,39 @@
+function removeRequest(id) {
+	let element = document.getElementById(id);
+	element.parentElement.removeChild(element);
+}
+
+function checkRequests() {
+	if ( $('#requestList').children().length == 0) {
+		$('#requestList').append('<small>You have no requests at this time.</small>');
+	}
+}
+
+function acceptRequest(id) {
+	removeRequest(id);
+
+	checkRequests();
+}
+
+function declineRequest(id) {
+	removeRequest(id);
+
+	checkRequests();
+}
+
+$(function() {
+      $('.dropdown').on({
+          "click": function(event) {
+            if ($(event.target).closest('.dropdown-toggle').length) {
+              $(this).data('closable', true);
+            } else {
+              $(this).data('closable', false);
+            }
+          },
+          "hide.bs.dropdown": function(event) {
+            hide = $(this).data('closable');
+            $(this).data('closable', true);
+            return hide;
+          }
+      });
+  });
